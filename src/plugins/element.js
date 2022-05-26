71182ba58a91042a069b81e36ac03d571ab30ba5
张星雨 <7396286+xing_yu_zhang@user.noreply.gitee.com>
2022-05-26 11:38:22 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 将弹框组件挂载到Vue的原型对象上
+Vue.prototype.$message = Message
